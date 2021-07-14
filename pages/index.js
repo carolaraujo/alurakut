fe4191c80c81dd4369aab9dfd9f9b9f717b077a4
Caroline Araujo <ca.araujosouza@gmail.com>
2021-07-14 10:09:48 -0300

    correção de bug 2

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
             <OrkutNostalgicIconSet />
           </Box>
         </div>
-        <div className="profilprofileRelationsArea" style={{ gridArea: 'profileprofileRelationsArea' }}>
+        <div className="profileprofileRelationsArea" style={{ gridArea: 'profileprofileRelationsArea' }}>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
